a4fbac247c915158223f4afd1faabfea9e088ca3
yunaa <yuna.ads@hotmail.com>
2024-09-05 16:58:52 -0300

    WordPress first commit

---
+++ patterns/footer.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: footer
+ * Description:
+ * Categories: footer
+ * Keywords:
+ * Viewport Width: 1500
+ * Block Types: core/template-part/footer
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"},"blockGap":"var:preset|spacing|x-large"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"main","textColor":"base","className":"dark-footer","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull dark-footer has-base-color has-main-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--xx-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-large","left":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600"><?php _e('Ride with WPEntrepreneur', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"main-accent","fontSize":"small"} -->
+<p class="has-main-accent-color has-text-color has-small-font-size"><?php _e('Easily create beautiful, fully-customizable websites with the new WordPress Site Editor and the WPEntrepreneur block theme. No coding skills required. Download for free today!', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"main","iconColorValue":"#150E29","iconBackgroundColor":"base","iconBackgroundColorValue":"#fff","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}},"className":"is-style-default","layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600"><?php _e('Company', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"textColor":"main-accent","layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-main-accent-color has-text-color has-small-font-size"><!-- wp:paragraph -->
+<p>About</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Careers</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Brand Assets</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600"><?php _e('Resources', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"textColor":"main-accent","layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-main-accent-color has-text-color has-small-font-size"><!-- wp:paragraph -->
+<p><?php _e('Blog', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Resources', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Support Docs', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Get Help', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600"><?php _e('Product', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"textColor":"main-accent","layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-main-accent-color has-text-color has-small-font-size"><!-- wp:paragraph -->
+<p><?php _e('Features', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Pricing', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Use Cases', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Demo', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:separator {"align":"wide","backgroundColor":"secondary","className":"is-style-separator-dotted"} -->
+<hr class="wp-block-separator alignwide has-text-color has-secondary-color has-alpha-channel-opacity has-secondary-background-color has-background is-style-separator-dotted"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"},"elements":{"link":{"color":{"text":"var:preset|color|main-accent"}}}},"textColor":"main-accent","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-main-accent-color has-text-color has-link-color"><!-- wp:paragraph {"fontSize":"x-small"} -->
+<p class="has-x-small-font-size"><?php _e('© 2023 · Powered by WordPress and', 'wpentrepreneur'); ?> <a href="https://wpwheels.com"><?php _e('WPEntrepreneur', 'wpentrepreneur'); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"x-small"} -->
+<div class="wp-block-group has-x-small-font-size"><!-- wp:paragraph -->
+<p><?php _e('Download', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Visit WPEntrepreneur', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e('Visit Mike', 'wpentrepreneur'); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ readme.txt
@@ -0,0 +1,86 @@
+=== WPEntrepreneur WordPress Block Theme ===
+Contributors: wpwheels
+Tags: blog, portfolio, entertainment, grid-layout, one-column, two-columns, three-columns, four-columns, block-patterns, block-styles, custom-logo, custom-menu, editor-style, featured-images, full-site-editing, full-width-template, style-variations, template-editing, theme-options, wide-blocks
+Requires at least: 6.2
+Tested up to: 6.6
+Requires PHP: 7.2
+Stable tag: 1.0.2
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+== Description ==
+
+WPEntrepreneur is an elegantly designed theme tailored for creating stunning wpentrepreneur websites. Ideal for those seeking a polished online presence, this theme boasts a lightweight, speed-optimized framework. Key highlights include eye-catching design patterns such as card layouts, a portfolio section, testimonials, and skill showcases. Perfect for showcasing entrepreneurial ventures with style and efficiency.
+
+
+== Copyright ==
+
+WPEntrepreneur Theme, (C) 2024 WPWheels
+WPEntrepreneur is distributed under the terms of the GNU GPL.
+
+WPEntrepreneur Theme bundles the following third-party resources:
+
+Font: PT Serif
+Copyright (c) 2010, ParaType Ltd.
+License: http://scripts.sil.org/OFL
+Source: https://github.com/google/fonts/tree/main/ofl/ptserif
+
+Font: Roboto Mono
+License: https://www.apache.org/licenses/LICENSE-2.0
+Source: https://github.com/googlefonts/RobotoMono
+
+== Image Used ==
+
+hero-banner.webp for hero-dark pattern, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/675849
+
+avatar-1.webp for Testiomonials, Credit Brooke Cagle
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://stocksnap.io/license
+Source: https://stocksnap.io/photo/people-man-A3WDGDTBI6
+
+pexels-chuck.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/1431757
+
+pexels-rachel-claire.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/1241547
+
+pexels-rozegold.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/102026
+
+pexels-shvetsa.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/892149
+
+pexels-zdmit.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/916925
+
+pexels-cottonbro.jpg for Portfolio Section, https://pxhere.com/
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+Source: https://pxhere.com/en/photo/1109951
+
+placeholder-image.jpg
+bg-texture.webp
+placeholder image and bg-texture image is crated by our own.
+license: https://creativecommons.org/publicdomain/zero/1.0/
+
+wow.min.js
+license: https://github.com/graingert/WOW/blob/master/LICENSE
+Source: https://github.com/graingert/WOW
+
+animate.min.css
+license: https://github.com/animate-css/animate.css/blob/main/LICENSE
+Source: https://github.com/animate-css/animate.css
+
